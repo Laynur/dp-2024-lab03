651f2d12b19dddf5809dcb63a732a76a21c3b959
Aynur <ainur2205@gmail.com>
2025-02-24 20:33:50 +0500

    fix(message): реализовн метод для возврата содержимого сообщения

--- message.py
+++ message.py
@@ -5,7 +5,12 @@ class Message(IMessage):
     """Класс базового сообщения"""
 
     def __init__(self, text: str):
-        self.text = text
+        self._text = text
 
     def Print(self):
-        print(self.text)
+        """Метод для вывода сообщения"""
+        print(self.get_content())
+
+    def get_content(self) -> str:
+        """Метод для возврата содержимого сообщения"""
+        return self._text
