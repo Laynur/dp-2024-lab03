082e8169ab240e8a2f164bdd80d9b659da882f3c
Aynur <ainur2205@gmail.com>
2025-02-24 21:45:07 +0500

    fix(main): главный файл

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from interface.imessage import IMessage
 from message import Message
+from decorators.header_decorator import HeaderDecorator
+from decorators.footer_decorator import FooterDecorator
+from decorators.date_decorator import DateDecorator
+from decorators.base_64_decorator import Base64Decorator
+# msg = Message("Пора идти на пересдачу")
+# msg.Print()
+#
+# msg = HeaderDecorator(Message("Пора идти на пересдачу"), "Дорогой мой друг!")
+# msg.Print()
+#
+# msg = FooterDecorator(HeaderDecorator(Message("Пора идти на пересдачу"), "Дорогой мой друг!"), "Твой брат")
+# msg.Print()
 
-msg = Message("С наступающим Новым годом!")
 
+# msg = DateDecorator(FooterDecorator(HeaderDecorator(Message("Пора идти на пересдачу"), "Дорогой мой друг!"), "Твой брат"))
+# msg.Print()
+
+msg = Base64Decorator(DateDecorator(FooterDecorator(HeaderDecorator(Message("Пора идти на пересдачу"), "Дорогой мой друг!"), "Твой брат")))
 msg.Print()
+
+
