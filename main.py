91245079b0bf109fe00af428e0eb3e51ab1b84f5
Aynur <ainur2205@gmail.com>
2025-03-04 13:01:58 +0500

    fix(main): Небольшие правки

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ from decorators.header_decorator import HeaderDecorator
 from decorators.footer_decorator import FooterDecorator
 from decorators.date_decorator import DateDecorator
 from decorators.base_64_decorator import Base64Decorator
-# msg = Message("Пора идти на пересдачу")
+
+# msg = Message("Коллеги")
 # msg.Print()
 #
-# msg = HeaderDecorator(Message("Пора идти на пересдачу"), "Дорогой мой друг!")
+# msg = HeaderDecorator(Message("Коллеги"), "Добрый день!")
 # msg.Print()
 #
-# msg = FooterDecorator(HeaderDecorator(Message("Пора идти на пересдачу"), "Дорогой мой друг!"), "Твой брат")
+# msg = FooterDecorator(HeaderDecorator(Message("Коллеги"), "Добрый день!"), "С уважением")
 # msg.Print()
 
 
-# msg = DateDecorator(FooterDecorator(HeaderDecorator(Message("Пора идти на пересдачу"), "Дорогой мой друг!"), "Твой брат"))
-# msg.Print()
+msg = DateDecorator(FooterDecorator(HeaderDecorator(Message("Коллеги"), "Добрый день!"), "С уважением"))
+msg.Print()
 
-msg = Base64Decorator(DateDecorator(FooterDecorator(HeaderDecorator(Message("Пора идти на пересдачу"), "Дорогой мой друг!"), "Твой брат")))
+msg = Base64Decorator(DateDecorator(FooterDecorator(HeaderDecorator(Message("Коллеги"), "Добрый день!"), "С уважением")))
 msg.Print()
 
 
